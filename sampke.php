8b9603c1b85e8de95bc54682f3b72e44c7b4eda2
Gideon Mark Pacete <gideonmarkcpacete@gmail.com>
2016-08-08 02:59:37 -0700

    why wilco page

--- sampke.php
+++ sampke.php
@@ -1,28 +1,103 @@
-<div class="row">
-	<div class="col-md-12">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis enim vel ligula tincidunt fringilla. Fusce vel consectetur purus.
+<div class="why_wilco">
+	<div class="row">
+		<div class="container">	
+			<hr>
+             <div >
+			<p class="why-wilco">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis enim vel ligula tincidunt fringilla. Fusce vel consectetur purus.
 Donec finibus, elit quis fermentum posuere, neque orci venenatis lacus,  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis enim vel ligula tincidunt fringilla. Fusce vel consectetur purus. Donec finibus, elit quis fermentum posuere, neque orci venenatis lacus, </p>
-	</div>
-	<div class="col-md-6">
-		<div class="col-md-12">
-			<img class="column-image" src="">
-			<span class="team-staff">
-				<p class="staff-name">Malcolm</p>
-				<p class="staff-position">Copy Writer</p>
-					<p>“I thoroughly recommend Wilco Hot Water. Got through on the phone immediately and they had a petrson out to repair our hot water system when they said they would. I’ve had no end of problems with tradies in the past but this group does what they say in a professional and friendly manner.”
-					</p>
-			</span>
+			</div>
 		</div>
 	</div>
-	<div class="col-md-6">
-		<div class="col-md-12">
-			<img class="column-image" src="">
-			<span class="team-staff">
-                <p class="staff-name">Malcolm</p>
-				<p class="staff-position">Copy Writer</p>
-				<p>“I thoroughly recommend Wilco Hot Water. Got through on the phone immediately and they had a petrson out to repair our hot water system when they said they would. I’ve had no end of problems with tradies in the past but this group does what they say in a professional and friendly manner.”
-				</p>
-			</span>
+	<div class="row alt-orange">
+		<div class="container">	
+			<div class="col-md-6">
+				<div class="col-md-2">
+					<img class="column-image" src="/wilcoblockeddrains/wp-content/uploads/2016/08/man3.png">
+				</div>
+				<div class="col-md-10">
+					<span class="testimonials">
+					<p>“I thoroughly recommend Wilco Hot Water. Got through on the phone immediately and they had a petrson out to repair our hot water system when they said they would. I’ve had no end of problems with tradies in the past but this group does what they say in a professional and friendly manner.”
+					<p class="testimonials-name">Malcolm</p>
+					<p class="testimonials-date">Thursday, 17th July 2016</p>
+					</p>
+					</span>
+				</div>
+			</div>	
+			<div class="col-md-6">
+				<div class="col-md-2">
+					<img class="column-image" src="/wilcoblockeddrains/wp-content/uploads/2016/08/man3.png">
+				</div>
+				<div class="col-md-10">
+					<span class="testimonials">
+					<p>“I thoroughly recommend Wilco Hot Water. Got through on the phone immediately and they had a petrson out to repair our hot water system when they said they would. I’ve had no end of problems with tradies in the past but this group does what they say in a professional and friendly manner.”
+					<p class="testimonials-name">Malcolm</p>
+					<p class="testimonials-date">Thursday, 17th July 2016</p>
+					</p>
+					</span>
+				</div>
+			</div>	
+		</div>
+	</div>
+	<div class="row">
+		<div class="container">	
+			<div class="col-md-6">
+				<div class="col-md-2">
+					<img class="column-image" src="/wilcoblockeddrains/wp-content/uploads/2016/08/man3.png">
+				</div>
+				<div class="col-md-10">
+					<span class="testimonials">
+					<p>“I thoroughly recommend Wilco Hot Water. Got through on the phone immediately and they had a petrson out to repair our hot water system when they said they would. I’ve had no end of problems with tradies in the past but this group does what they say in a professional and friendly manner.”
+					<p class="testimonials-name">Malcolm</p>
+					<p class="testimonials-date">Thursday, 17th July 2016</p>
+					</p>
+					</span>
+				</div>
+			</div>	
+			<div class="col-md-6">
+				<div class="col-md-2">
+					<img class="column-image" src="/wilcoblockeddrains/wp-content/uploads/2016/08/man3.png">
+				</div>
+				<div class="col-md-10">
+					<span class="testimonials">
+					<p>“I thoroughly recommend Wilco Hot Water. Got through on the phone immediately and they had a petrson out to repair our hot water system when they said they would. I’ve had no end of problems with tradies in the past but this group does what they say in a professional and friendly manner.”
+					<p class="testimonials-name">Malcolm</p>
+					<p class="testimonials-date">Thursday, 17th July 2016</p>
+					</p>
+					</span>
+				</div>
+			</div>	
+		</div>
+	</div>
+	<div class="row alt-orange">
+		<div class="container">	
+			<div class="col-md-6">
+				<div class="col-md-2">
+					<img class="column-image" src="/wilcoblockeddrains/wp-content/uploads/2016/08/man3.png">
+				</div>
+				<div class="col-md-10">
+					<span class="testimonials">
+					<p>“I thoroughly recommend Wilco Hot Water. Got through on the phone immediately and they had a petrson out to repair our hot water system when they said they would. I’ve had no end of problems with tradies in the past but this group does what they say in a professional and friendly manner.”
+					<p class="testimonials-name">Malcolm</p>
+					<p class="testimonials-date">Thursday, 17th July 2016</p>
+					</p>
+					</span>
+				</div>
+			</div>	
+			<div class="col-md-6">
+				<div class="col-md-2">
+					<img class="column-image" src="/wilcoblockeddrains/wp-content/uploads/2016/08/man3.png">
+				</div>
+				<div class="col-md-10">
+					<span class="testimonials">
+					<p>“I thoroughly recommend Wilco Hot Water. Got through on the phone immediately and they had a petrson out to repair our hot water system when they said they would. I’ve had no end of problems with tradies in the past but this group does what they say in a professional and friendly manner.”
+					<p class="testimonials-name">Malcolm</p>
+					<p class="testimonials-date">Thursday, 17th July 2016</p>
+					</p>
+					</span>
+				</div>
+			</div>	
 		</div>
 	</div>
 </div>
+
+
