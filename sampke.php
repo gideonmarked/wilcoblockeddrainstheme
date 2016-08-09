b1210921331e2a78336feb10b066a712e13e6e04
Gideon Mark Pacete <gideonmarkcpacete@gmail.com>
2016-08-09 15:29:07 -0700

    recent work page

--- sampke.php
+++ sampke.php
@@ -1,103 +1,57 @@
-<div class="why_wilco">
-	<div class="row">
-		<div class="container">	
-			<hr>
-             <div >
-			<p class="why-wilco">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis enim vel ligula tincidunt fringilla. Fusce vel consectetur purus.
-Donec finibus, elit quis fermentum posuere, neque orci venenatis lacus,  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis enim vel ligula tincidunt fringilla. Fusce vel consectetur purus. Donec finibus, elit quis fermentum posuere, neque orci venenatis lacus, </p>
+<div class="wilco-previous">
+	<div class="container">
+		<div class="row">
+			<h2 class="previous-title-header">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+			<div class="col-md-6">
+        		<img class="previous-column-image-header" src="http://wilcoblockeddrains.com.au/wp-content/uploads/2016/08/news_image1.png">
+        	</div>
+        	<div class="col-md-6">
+	        	<span class="previous-column-description">
+					<p class="previous-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec consectetur dui. Suspendisse in ipsum eget lectus efficitur porta. Phasellus eget euismod orci. Ut vestibulum scelerisque mi. Vestibulum scelerisque sodales magna quis pulvinar. Vestibulum blandit, risus eu gravida tempor, nulla ex fermentum odio, accumsan maximus lacus erat nec tellus. Nulla facilisi. Suspendisse hendererit ac neque a vestibulum. Morbi purus nisl, congue id metus eu, venenatis congue massa. Phasellus non nunc bibendum, volutpat mauris vitae, ultricies est. Nulla facilisi.
+					</p>
+					<p class="previous-time-taken">Time taken to complete: 24hrs</p>
+					<p class="previous-suburb">Suburb: Sydney</p>
+				</span>
+			</div>	        
+		</div>
+
+
+		<div class="row previous-image">
+	        <div class="previous-column-holder col-md-4">
+	        	<a href="#" class="previous-link"></a>
+	        	<img class="previous-column-image" src="http://wilcoblockeddrains.com.au/wp-content/uploads/2016/08/news_image4.png">
+	        </div>
+	        <div class="previous-column-holder col-md-4">
+	            <a href="#" class="previous-link"></a>
+	            <img class="previous-column-image" src="http://wilcoblockeddrains.com.au/wp-content/uploads/2016/08/news_image2.png">			
+	        </div>
+	        <div class="previous-column-holder col-md-4">
+	        	<a href="#" class="previous-link"></a>
+	        	<img class="previous-column-image" src="http://wilcoblockeddrains.com.au/wp-content/uploads/2016/08/news_image3.png">
+	        </div>
+	    </div>
+	</div>
+</div>
+	
+<div class="previous-work-testimonials">
+	<div class="container">
+		<div class="row">		    	    
+			<div class="col-md-2">
+				<img class="previous-testimonials-image" src="http://wilcoblockeddrains.com.au/wp-content/uploads/2016/08/man3.png">
+			</div>
+			<div class="col-md-10 previous-testimonials">
+				<h2 class="previous-testimonials-title">Testimonials</h2>
+		    	<p class="previous-testimonials-title-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>	
+				<span class="previous-testimonials">
+				<p class="previous-testimonials-content">Aenean vestibulum, enim et aliquet ullamcorper, nisi metus ultrices lectus, in pharetra nunc massa ut sem. Aliquam a lacinia mi. Sed vitae ipsum mollis, vulputate sapien a, feugiat velit. Etiam at tincidunt velit, in commodo tortor. Ut sit amet sollicitudin est. Maecenas a justo dapibus, mattis massa non, consectetur ligula. Pellentesque erat purus, congue quis molestie ut, tritique aliquet elit. Suspendisse tincidunt phareta libero vel pharetra. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vestibulum, enim et aliquet ullamcorper, nisi metus ultrices lectus, in phareta nunc massa ut sem. Aliquam a lacinia mi. Sed vitae ipsum mollis, vulputate sapien a, feugiat velit. Etiam at tincidunt velit, in commodo torotr. Ut sit amet sollicitudin est. Maecenas a justo dapibus, mattis massa non, consectetur ligula. Pellentesque erat purus, congue quis molestie ut, tristique aliquet elit. Suspendisse tincidunt pharetta libero vel pharetra.
+				<p class="previous-testimonials-name">Malcolm</p>
+				<p class="previous-testimonials-date">Thursday, 17th July 2016</p>
+				</p>
+				</span>
 			</div>
 		</div>
-	</div>
-	<div class="row alt-orange">
-		<div class="container">	
-			<div class="col-md-6">
-				<div class="col-md-2">
-					<img class="column-image" src="/wilcoblockeddrains/wp-content/uploads/2016/08/man3.png">
-				</div>
-				<div class="col-md-10">
-					<span class="testimonials">
-					<p>“I thoroughly recommend Wilco Hot Water. Got through on the phone immediately and they had a petrson out to repair our hot water system when they said they would. I’ve had no end of problems with tradies in the past but this group does what they say in a professional and friendly manner.”
-					<p class="testimonials-name">Malcolm</p>
-					<p class="testimonials-date">Thursday, 17th July 2016</p>
-					</p>
-					</span>
-				</div>
-			</div>	
-			<div class="col-md-6">
-				<div class="col-md-2">
-					<img class="column-image" src="/wilcoblockeddrains/wp-content/uploads/2016/08/man3.png">
-				</div>
-				<div class="col-md-10">
-					<span class="testimonials">
-					<p>“I thoroughly recommend Wilco Hot Water. Got through on the phone immediately and they had a petrson out to repair our hot water system when they said they would. I’ve had no end of problems with tradies in the past but this group does what they say in a professional and friendly manner.”
-					<p class="testimonials-name">Malcolm</p>
-					<p class="testimonials-date">Thursday, 17th July 2016</p>
-					</p>
-					</span>
-				</div>
-			</div>	
-		</div>
-	</div>
-	<div class="row">
-		<div class="container">	
-			<div class="col-md-6">
-				<div class="col-md-2">
-					<img class="column-image" src="/wilcoblockeddrains/wp-content/uploads/2016/08/man3.png">
-				</div>
-				<div class="col-md-10">
-					<span class="testimonials">
-					<p>“I thoroughly recommend Wilco Hot Water. Got through on the phone immediately and they had a petrson out to repair our hot water system when they said they would. I’ve had no end of problems with tradies in the past but this group does what they say in a professional and friendly manner.”
-					<p class="testimonials-name">Malcolm</p>
-					<p class="testimonials-date">Thursday, 17th July 2016</p>
-					</p>
-					</span>
-				</div>
-			</div>	
-			<div class="col-md-6">
-				<div class="col-md-2">
-					<img class="column-image" src="/wilcoblockeddrains/wp-content/uploads/2016/08/man3.png">
-				</div>
-				<div class="col-md-10">
-					<span class="testimonials">
-					<p>“I thoroughly recommend Wilco Hot Water. Got through on the phone immediately and they had a petrson out to repair our hot water system when they said they would. I’ve had no end of problems with tradies in the past but this group does what they say in a professional and friendly manner.”
-					<p class="testimonials-name">Malcolm</p>
-					<p class="testimonials-date">Thursday, 17th July 2016</p>
-					</p>
-					</span>
-				</div>
-			</div>	
-		</div>
-	</div>
-	<div class="row alt-orange">
-		<div class="container">	
-			<div class="col-md-6">
-				<div class="col-md-2">
-					<img class="column-image" src="/wilcoblockeddrains/wp-content/uploads/2016/08/man3.png">
-				</div>
-				<div class="col-md-10">
-					<span class="testimonials">
-					<p>“I thoroughly recommend Wilco Hot Water. Got through on the phone immediately and they had a petrson out to repair our hot water system when they said they would. I’ve had no end of problems with tradies in the past but this group does what they say in a professional and friendly manner.”
-					<p class="testimonials-name">Malcolm</p>
-					<p class="testimonials-date">Thursday, 17th July 2016</p>
-					</p>
-					</span>
-				</div>
-			</div>	
-			<div class="col-md-6">
-				<div class="col-md-2">
-					<img class="column-image" src="/wilcoblockeddrains/wp-content/uploads/2016/08/man3.png">
-				</div>
-				<div class="col-md-10">
-					<span class="testimonials">
-					<p>“I thoroughly recommend Wilco Hot Water. Got through on the phone immediately and they had a petrson out to repair our hot water system when they said they would. I’ve had no end of problems with tradies in the past but this group does what they say in a professional and friendly manner.”
-					<p class="testimonials-name">Malcolm</p>
-					<p class="testimonials-date">Thursday, 17th July 2016</p>
-					</p>
-					</span>
-				</div>
-			</div>	
-		</div>
-	</div>
+    </div>
 </div>
 
 
+		
